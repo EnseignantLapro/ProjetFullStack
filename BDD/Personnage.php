3b401a18c1b7d6d054739e3443e2fc472b80b40a
Clement80260 <55393987+Clement80260@users.noreply.github.com>
2020-03-12 11:58:53 +0100

    test

--- BDD/Personnage.php
+++ BDD/Personnage.php
@@ -9,7 +9,7 @@ class Personnage{
     public function __construct($id){
         //récupérer le bon personnage avec son id
         //simule un user
-        $this->_nom = "julien";
+        $this->_nom = "test";
 
     }
 
