0fa0c3c083c4ec11b59ee117bc10666ca0176005
Wantelez <wantelet@example.com>
2020-03-12 11:48:31 +0100

    first commit wantelez

--- Metier/combat.php
+++ Metier/combat.php
@@ -1,8 +1,11 @@
-<?php include "../BDD/Personnage.php";
+<?php
+//DEV BY WANTELEZ LE 12/03/84
+
+include "../BDD/Personnage.php";
 
 
 function combat($Perso1,$Perso2){
-
+    echo "je suis wantelez";
     echo "combat de ".$Perso1->afficherInfo()." et de ".$Perso2->afficherInfo();
 
 
