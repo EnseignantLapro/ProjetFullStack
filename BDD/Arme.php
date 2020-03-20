3f9f2eafa45a579961cf2ce1b1f677ae8ac431ac
Florian Garcia <fgarcia@la-providence.net>
2020-03-20 15:39:46 +0100

    1

---
+++ BDD/Arme.php
@@ -0,0 +1,32 @@
+<?php
+class Arme
+{
+        private $_id;
+        private $_Nom;
+
+        public function __construct($id,$Nom){
+        $this->_id = $id;
+        $this->_Nom = $Nom;
+        }
+
+        public function getId(){
+        return $this->_id;
+        }
+
+        public function getNom(){
+        return $this->_Nom;
+        }
+
+        public function AfficheArme(){
+            echo $this->_Nom.' équipé | ID Arme:'.$this->_id;
+        }
+
+        public function deleteArme()
+        {
+            echo "ID Arme suppression: " . $this->_id . " Arme supprimer: " . $this->_Nom;
+            global $Bdd;
+            $delet = $Bdd->prepare("DELETE FROM `arme` WHERE `id_arme` = " . $this->_id);
+            $delet->execute(array($this->_id));
+        }
+ }
+?>
