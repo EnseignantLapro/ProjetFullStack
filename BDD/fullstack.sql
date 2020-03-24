c3a7d2f504409be91a910d67af04f38105d8e245
florian <fwantelez@la-providence.net>
2020-03-24 18:20:50 +0100

    new bdd

--- BDD/fullstack.sql
+++ BDD/fullstack.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 23 mars 2020 à 21:41
+-- Généré le :  mar. 24 mars 2020 à 18:19
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -36,7 +36,46 @@ CREATE TABLE IF NOT EXISTS `arme` (
   `durabilite` int(11) NOT NULL,
   `bonus_degat` int(11) NOT NULL,
   PRIMARY KEY (`id_arme`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `arme`
+--
+
+INSERT INTO `arme` (`id_arme`, `nom`, `prix`, `durabilite`, `bonus_degat`) VALUES
+(1, 'NULL', 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `assoshero`
+--
+
+DROP TABLE IF EXISTS `assoshero`;
+CREATE TABLE IF NOT EXISTS `assoshero` (
+  `id_assoshero` int(11) NOT NULL AUTO_INCREMENT,
+  `id_user` int(11) NOT NULL,
+  `id_hero` int(11) NOT NULL,
+  `id_arme` int(11) NOT NULL,
+  `potion` int(11) NOT NULL,
+  `pdv` float NOT NULL,
+  `attaque` float NOT NULL,
+  `defense` float NOT NULL,
+  `niveau` int(11) NOT NULL,
+  PRIMARY KEY (`id_assoshero`),
+  UNIQUE KEY `id_user_2` (`id_user`,`id_hero`,`id_arme`),
+  KEY `id_user` (`id_user`),
+  KEY `id_hero` (`id_hero`),
+  KEY `id_arme` (`id_arme`),
+  KEY `id_user_3` (`id_user`,`id_hero`,`id_arme`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `assoshero`
+--
+
+INSERT INTO `assoshero` (`id_assoshero`, `id_user`, `id_hero`, `id_arme`, `potion`, `pdv`, `attaque`, `defense`, `niveau`) VALUES
+(1, 1, 2, 1, 0, 650, 20, 30, 1);
 
 -- --------------------------------------------------------
 
@@ -46,39 +85,21 @@ CREATE TABLE IF NOT EXISTS `arme` (
 
 DROP TABLE IF EXISTS `mob`;
 CREATE TABLE IF NOT EXISTS `mob` (
-  `id_mod` int(11) NOT NULL AUTO_INCREMENT,
+  `id_mob` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(15) NOT NULL,
   `pdv` float NOT NULL,
   `attaque` float NOT NULL,
   `defence` float NOT NULL,
   `etat` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id_mod`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
+  PRIMARY KEY (`id_mob`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 
 --
--- Structure de la table `tableassociation`
+-- Déchargement des données de la table `mob`
 --
 
-DROP TABLE IF EXISTS `tableassociation`;
-CREATE TABLE IF NOT EXISTS `tableassociation` (
-  `id_assoc` int(11) NOT NULL AUTO_INCREMENT,
-  `id_user` int(11) NOT NULL,
-  `id_hero` int(11) NOT NULL,
-  `id_arme` int(11) NOT NULL,
-  `potion` int(11) NOT NULL,
-  `pdv` float NOT NULL,
-  `attaque` float NOT NULL,
-  `defense` float NOT NULL,
-  `niveau` int(11) NOT NULL,
-  PRIMARY KEY (`id_assoc`),
-  UNIQUE KEY `id_user_2` (`id_user`,`id_hero`,`id_arme`),
-  KEY `id_user` (`id_user`),
-  KEY `id_hero` (`id_hero`),
-  KEY `id_arme` (`id_arme`),
-  KEY `id_user_3` (`id_user`,`id_hero`,`id_arme`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `mob` (`id_mob`, `nom`, `pdv`, `attaque`, `defence`, `etat`) VALUES
+(1, 'Dragon', 3695, 80, 180, 1);
 
 -- --------------------------------------------------------
 
@@ -97,7 +118,14 @@ CREATE TABLE IF NOT EXISTS `typehero` (
   `categorie` int(11) NOT NULL,
   PRIMARY KEY (`id_hero`),
   KEY `id_arme` (`id_arme`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `typehero`
+--
+
+INSERT INTO `typehero` (`id_hero`, `nom`, `id_arme`, `pdv`, `attaque`, `defence`, `categorie`) VALUES
+(2, 'Ashe', 1, 650, 20, 30, 1);
 
 -- --------------------------------------------------------
 
@@ -112,19 +140,26 @@ CREATE TABLE IF NOT EXISTS `user` (
   `mail` varchar(30) NOT NULL,
   `mdp` varchar(100) NOT NULL,
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id_user`, `pseudo`, `mail`, `mdp`) VALUES
+(1, 'Rainolf', 'floflobg1999@hotmail.fr', 'mdpsecure123');
 
 --
 -- Contraintes pour les tables déchargées
 --
 
 --
--- Contraintes pour la table `tableassociation`
+-- Contraintes pour la table `assoshero`
 --
-ALTER TABLE `tableassociation`
-  ADD CONSTRAINT `tableassociation_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
-  ADD CONSTRAINT `tableassociation_ibfk_2` FOREIGN KEY (`id_hero`) REFERENCES `typehero` (`id_hero`),
-  ADD CONSTRAINT `tableassociation_ibfk_3` FOREIGN KEY (`id_arme`) REFERENCES `arme` (`id_arme`);
+ALTER TABLE `assoshero`
+  ADD CONSTRAINT `assoshero_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`),
+  ADD CONSTRAINT `assoshero_ibfk_2` FOREIGN KEY (`id_hero`) REFERENCES `typehero` (`id_hero`),
+  ADD CONSTRAINT `assoshero_ibfk_3` FOREIGN KEY (`id_arme`) REFERENCES `arme` (`id_arme`);
 
 --
 -- Contraintes pour la table `typehero`
