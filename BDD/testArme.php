c6b94beb1c032d5ca7f1d18a0479e3a7e225591d
jaeparc <55393528+jaeparc@users.noreply.github.com>
2020-03-24 23:30:17 +0100

    suppression formulaire choix user test arme

--- BDD/testArme.php
+++ BDD/testArme.php
@@ -3,23 +3,11 @@
 include("../header.php");
 include("Arme.php");
 ?>
-<!-- Choix du "user" -->
-<form action="" method="post">
-    <select name="Listeuser">
-        <?php
-        $reqUser = $Bdd->query("SELECT * FROM user");
-        while ($userinfo = $reqUser->fetch()) {
-            echo "<option value=" . $userinfo['id_user'] . ">" . $userinfo['pseudo'] . "</option>";
-        } ?>
-    </select>
-    <input type="submit" name="submitUser">
-</form>
-<?php if (isset($_POST['submitUser'])) { $id_user = $_POST['Listeuser'];?>
     <!-- Choix du heros et choix de l'arme à équiper -->
     <form action="" method="post">
         <select name="Listeheros">
             <?php
-            $reqAssos = $Bdd->query("SELECT * FROM assoshero WHERE id_user = " . $_POST['Listeuser'] . "");
+            $reqAssos = $Bdd->query("SELECT * FROM assoshero WHERE id_user = 1");
             while ($info = $reqAssos->fetch()) {
                 $reqHero = $Bdd->query('SELECT * FROM typehero WHERE id_hero = ' . $info['id_hero'] . '');
                 $heroinfo = $reqHero->fetch();
@@ -36,12 +24,11 @@ include("Arme.php");
         <input type="submit" name="submitArme">
     </form>
 <?php
-}
 // Equipement de l'arme
 if (isset($_POST['submitArme'])) {
     $arme = new Arme($Bdd, $_POST['Listearmes']);
     echo "Le nom de l'arme est : " . $arme->getNom() . ", elle coûte " . $arme->getPrix() . "€ et elle fait " . $arme->getDegat() . " de dégats";
     echo "<br>Arme équipée";
-    $arme->equiperArme($id_user, $_POST['Listeheros']);
+    $arme->equiperArme(1, $_POST['Listeheros']);
 }
 ?>
