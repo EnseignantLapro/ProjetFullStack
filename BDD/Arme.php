8f24caa68961d2a82add9da713fed303f00775f4
FGarcia98 <55394427+FGarcia98@users.noreply.github.com>
2020-03-26 10:52:13 +0100

    Update Arme.php

--- BDD/Arme.php
+++ BDD/Arme.php
@@ -3,30 +3,69 @@ class Arme
 {
         private $_id;
         private $_Nom;
+        private $_Prix;
+        private $_Durabilite;
+        private $_Degat;
 
-        public function __construct($id,$Nom){
-        $this->_id = $id;
-        $this->_Nom = $Nom;
+        public function __construct($id, $Nom, $Prix, $Durabilite, $Degat)
+        {
+                $this->_id = $id;
+                $this->_Nom = $Nom;
+                $this->_Prix = $Prix;
+                $this->_Durabilite = $Durabilite;
+                $this->_Degat = $Degat;
         }
-//id Arme
-        public function getId(){
-        return $this->_id;
+        //id Arme
+        public function getId()
+        {
+                return $this->_id;
         }
-//Nom Arme 
-        public function getNom(){
-        return $this->_Nom;
+        //Nom Arme 
+        public function getNom()
+        {
+                return $this->_Nom;
         }
-//Affiche le nom et l'id de l'arme
-        public function AfficheArme(){
-            echo $this->_Nom.' équipé | ID Arme:'.$this->_id;
+        //Prix arme
+        public function getPrix()
+        {
+                return $this->_Prix;
         }
-//Supprime l'arme dans la BDD
+        //Bonus durabilité
+        public function getDurabilite()
+        {
+                return $this->_Durabilite;
+        }
+        //Bonus dégat arme
+        public function getDegat()
+        {
+                return $this->_Degat;
+        }
+        //Affiche nom / id / Prix / durabilité / dégat de l'arme
+        public function AfficheArme()
+        {
+                echo 'ID Arme:' . $this->_id . ' | Arme:' . $this->_Nom.' | prix:'.$this->_Prix.'$ | Durabilité:'.$this->_Durabilite.' | Dégat :'.$this->_Degat ;
+        }
+        //Supprime l'arme dans la BDD
         public function deleteArme()
         {
-            echo "ID Arme suppression: " . $this->_id . " Arme supprimer: " . $this->_Nom;
-            global $Bdd;
-            $delet = $Bdd->prepare("DELETE FROM `arme` WHERE `id_arme` = " . $this->_id);
-            $delet->execute(array($this->_id));
+                echo "ID Arme suppression: " . $this->_id . " Arme supprimer: " . $this->_Nom;
+                global $Bdd;
+                $delet = $Bdd->prepare("DELETE FROM `arme` WHERE `id_arme` = " . $this->_id);
+                $delet->execute(array($this->_id));
         }
- }
-?>
+
+        //Craft arme
+        public function craftArme($Nom,$Prix,$Durabilite,$Degat)
+        {
+                global $Bdd;
+                $ajout = $Bdd->prepare("INSERT INTO `arme` (`nom_arme`, `prix`, `bonus_durabilite`, `bonus_degat`)
+                 VALUES (:nom_arme,:prix,:bonus_durabilite,:bonus_degat)");
+                $ajout->execute(['nom_arme' => $Nom,
+                'prix' => $Prix,
+                'bonus_durabilite' => $Durabilite,
+                'bonus_degat' => $Degat
+                ]);
+
+   
+        }
+}
