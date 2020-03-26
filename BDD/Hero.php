305597a25555a5a5de6622ae2de686ebc72daa0b
florian <fwantelez@la-providence.net>
2020-03-26 10:33:24 +0100

    fonction attaque Hero

--- BDD/Hero.php
+++ BDD/Hero.php
@@ -7,9 +7,10 @@ class Hero
 
     private $_Id;
     private $_VieMob;
-    private $_Attaque;
+    private $_AttaqueHero;
     private $_Bdd;
-    private $_Vie;
+    private $_VieHero;
+    private $_AttaqueMob;
 
 
     public function __construct($IdDuPseudo, $Bdd)
@@ -21,17 +22,16 @@ class Hero
             $DataPersonnage = $this->_Bdd->query("SELECT * from assoshero where id_assoshero =" . $IdDuPseudo . "");
             $TabDataPersonnage = $DataPersonnage->fetch();
             $this->_Id = $IdDuPseudo;
-            $this->_Vie = $TabDataPersonnage['pdv'];
-            $this->_Attaque = $TabDataPersonnage['attaque'];
+            $this->_VieHero = $TabDataPersonnage['pdv'];
+            $this->_AttaqueHero = $TabDataPersonnage['attaque'];
             $DataMob = $this->_Bdd->query("SELECT * from mob where id_mob =" . $IdDuPseudo . "");
             $TabDataMob = $DataMob->fetch();
             $this->_VieMob = $TabDataMob['pdv'];
-
         } else {
             //verifier que id est coorrect sinon simulé un perso
             $this->_Id = 0;
             $this->_VieMob = 1000;
-            $this->_Attaque = 50;
+            $this->_AttaqueHero = 50;
         }
     }
 
@@ -44,7 +44,7 @@ class Hero
         $TabdDataMonstre = $DataMonstre->fetch();
 
         // on lui soustrais l'attaque du héro
-        $NewVieMonstre = $TabdDataMonstre['pdv'] - $this->_Attaque;
+        $NewVieMonstre = $TabdDataMonstre['pdv'] - $this->_AttaqueHero;
 
         // Si ça est inférieure à 0 ou null alors il est mort on passe son état 0
         if ($NewVieMonstre <= 0) {
@@ -59,6 +59,25 @@ class Hero
             $TabDataMob = $DataMob->fetch();
             $this->_VieMob = $TabDataMob['pdv'];
         }
+    }
+
+    public function AttaqueHero($IdHero)
+    {
+// On retire la vie du Hero par les degats d'attaques du Mob
+        $NewVieHero = $this->_VieHero - $this->_AttaqueMob;
+        if ($NewVieHero <= 0) {
+
+            $this->_Bdd->query("UPDATE eta set etat = 0 WHERE id_mob =" . $IdHero . "");
+            $this->_Bdd->query("UPDATE eta set pdv = 0 WHERE id_mob =" . $IdHero . "");
+            $this->_VieMob = 0;
+        } else {
+            // Sinon on acualise ses point de vie à ses points de vie moins les dégats de l'attaquant
+            $this->_Bdd->query("UPDATE mob set pdv =" . $NewVieHero . " WHERE id_mob =" . $IdHero . "");
+            $DataMob = $this->_Bdd->query("SELECT * from mob where id_mob = " . $IdHero . "");
+            $TabDataMob = $DataMob->fetch();
+            $this->_VieHero = $TabDataHero['pdv'];
+        }
+
     }
     // Fonction qui permet de remettre les points de vie à 100
     public function ResetHp($IdMob)
@@ -78,7 +97,6 @@ class Hero
     function GetVieMob()
     {
         return $this->_VieMob;
-
     }
 
     // Dev By Fresi
@@ -93,6 +111,6 @@ class Hero
     public function AfficherStats()
     {
 
-        echo "Votre pseudo est " . $this->_pseudo . " Vous avez " . $this->_vie . " point de vie et " . $this->_attaque . " d'attaque";
+        echo "Votre pseudo est " . $this->_pseudo . " Vous avez " . $this->_vie . " point de vie et " . $this->_AttaqueHero . " d'attaque";
     }
 }
