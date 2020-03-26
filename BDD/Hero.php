1cb8fc9dd70acef45bc6ecaa2c631fb7c7e361ee
Flaquet <eflaquet@la-providence.net>
2020-03-26 10:47:21 +0100

    z

--- BDD/Hero.php
+++ BDD/Hero.php
@@ -6,52 +6,77 @@ class Hero
 {
 
     private $_Id;
-    private $_Vie;
-    private $_Attaque;
+    private $_VieMob;
+    private $_AttaqueHero;
     private $_Bdd;
+    private $_Vie;
+    private $_User;
+    private $_NomHero;
+    private $_Arme;
+    private $_Armure;
 
 
     public function __construct($IdDuPseudo, $Bdd)
     {
         $this->_Bdd = $Bdd;
-        //Langlace ce rajoute un control si la bdd existe pas je la simule
-        if(!is_null($Bdd)){
-            $DataPersonnage = $this->_Bdd->query("SELECT * from personnage where id =" . $IdDuPseudo . "");
+        //rajoute un control si la bdd existe pas je la simule
+        if (!is_null($Bdd)) {
+            //go to base chercher les info du personnages par id
+            $DataPersonnage = $this->_Bdd->query("SELECT * from assoshero where id_assoshero =" . $IdDuPseudo . "");
             $TabDataPersonnage = $DataPersonnage->fetch();
             $this->_Id = $IdDuPseudo;
-            $this->_Vie = $TabDataPersonnage['vie'];
+            $this->_Vie = $TabDataPersonnage['pdv'];
             $this->_Attaque = $TabDataPersonnage['attaque'];
-        }else{
+            $DataMob = $this->_Bdd->query("SELECT * from mob where id_mob =" . $IdDuPseudo . "");
+            $TabDataMob = $DataMob->fetch();
+            $this->_VieMob = $TabDataMob['pdv'];
+            $DataUser = $this->_Bdd->query("SELECT * from user where id_user =" . $IdDuPseudo . "");
+            $TabDataUser = $DataUser->fetch();
+            $this->_NomHero = $TabDataUser['pseudo'];
+            $DataArme = $this->_Bdd->query("SELECT * from arme where id_arme = " . $IdDuPseudo . "");
+            $TabdDataPerso = $DataArme->fetch();
+            $this->_Arme = $TabdDataPerso['nom'];
+            $DataArmure = $this->_Bdd->query("SELECT * from armure where id_armure = " . $IdDuPseudo . "");
+            $TabdDataPerso = $DataArmure->fetch();
+            $this->_Armure = $TabdDataPerso['bonus_armure'];
+
+        } else {
+            //verifier que id est coorrect sinon simulé un perso
             $this->_Id = 0;
-            $this->_Vie = 10;
-            $this->_Attaque = 10;
+            $this->_VieMob = 1000;
+            $this->_Attaque = 50;
         }
-        
-
-        //go to base chercher les info du personnages par id
-
-
-        //verifier que id est coorrect sinon simulé un perso
-
-        $this->_Pseudo = "Perso Simulé N°" . $IdDuPseudo;
-        $this->_Vie = 50;  // La vie sera prédéfini à celle du niveau 1.
-        $this->_Attaque = 5;  // L'attaque sera défini à celle du niveau 1.
     }
 
     // Dev by Wantelez Florian //
     // Fonction qui permet de soustraire les points de vie de la cible en fonction des point d'attaques du personnage
     public function AttaqueMob($IdMob)
     {
-        $DataMonstre = $this->_Bdd->query("SELECT * from monstre where id_monstre =" . $IdMob . "");
+        //on selectionne la vie du monstre
+        $DataMonstre = $this->_Bdd->query("SELECT * from mob where id_mob =" . $IdMob . "");
         $TabdDataMonstre = $DataMonstre->fetch();
-        $NewVieMonstre = $TabdDataMonstre['vie'] - $this->_Attaque;
 
+        // on lui soustrais l'attaque du héro
+        $NewVieMonstre = $TabdDataMonstre['pdv'] - $this->_Attaque;
+
+        // Si ça est inférieure à 0 ou null alors il est mort on passe son état 0
         if ($NewVieMonstre <= 0) {
 
-            $this->_Bdd->query("UPDATE monstre set etat = 'mort' WHERE id_monstre =" . $IdMob . "");
+            $this->_Bdd->query("UPDATE mob set etat = 0 WHERE id_mob =" . $IdMob . "");
+            $this->_Bdd->query("UPDATE mob set pdv = 0 WHERE id_mob =" . $IdMob . "");
+            $this->_VieMob = 0;
         } else {
-            $this->_Bdd->query('UPDATE monstre set vie = '.$NewVieMonstre.' WHERE id_monstre='.$IdMob."");
+            // Sinon on acualise ses point de vie à ses points de vie moins les dégats de l'attaquant
+            $this->_Bdd->query("UPDATE mob set pdv =" . $NewVieMonstre . " WHERE id_mob =" . $IdMob . "");
+            $DataMob = $this->_Bdd->query("SELECT * from mob where id_mob = " . $IdMob . "");
+            $TabDataMob = $DataMob->fetch();
+            $this->_VieMob = $TabDataMob['pdv'];
         }
+    }
+    // Fonction qui permet de remettre les points de vie à 100
+    public function ResetHp($IdMob)
+    {
+        $this->_Bdd->query("UPDATE mob set pdv = 100 WHERE id_mob=" . $IdMob . "");
     }
 
     // Dev By Fresi
@@ -63,23 +88,37 @@ class Hero
 
     // Dev By Fresi
     // Fonction qui renvoie la valeur de la vie
-    function GetVie()
+    function GetVieMob()
     {
-        return $this->_Vie;
+        return $this->_VieMob;
+
     }
 
     // Dev By Fresi
     // Fonction pour changer de Pseudo
-    function SetPseudo($NouveauSpoeudo)
+    function SetPseudo($NouveauPseudo)
     {
-        return $this->_Pseudo = $NouveauSpoeudo;
+        return $this->_NomHero = $NouveauPseudo;
     }
 
     // Dev By Fresi
     // Fonction qui affiche les statistiques d'un personnage
     public function AfficherStats()
     {
+        echo "Votre pseudo est " . $this->_NomHero . " Vous avez " . $this->_Vie . " point de vie et " . $this->_AttaqueHero . " d'attaque";
+    }
 
-        echo "Votre pseudo est " . $this->_pseudo . " Vous avez " . $this->_vie . " point de vie et " . $this->_attaque . " d'attaque";
+    // Dev By Fresi
+    // Fonction qui affiche l'arme d'un personnage
+    public function AfficherArme()
+    {
+        return $this->_Arme;
+    }
+
+    // Dev By Fresi
+    // Fonction qui affiche l'armure d'un personnage
+    public function AfficherArmure()
+    {
+        return $this->_Armure;
     }
 }
