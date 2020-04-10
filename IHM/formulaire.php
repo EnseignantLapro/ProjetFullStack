c4fb991288807ca860c66552749e00d1e7ee4879
florian <fwantelez@la-providence.net>
2020-04-10 02:56:59 +0200

    review code Val

--- IHM/formulaire.php
+++ IHM/formulaire.php
@@ -1,95 +1,104 @@
-<?php require("./BDD/connexion.php");?>
+<?php require("../BDD/connexion.php");
+require "../header.php"; ?>
 
 <html>
-    <head>
-        
-    </head>
 
-        <body>
-            <div class="container" align="center">
-                <div class="row">
+<head>
 
-                    <div class="col-lg-12 fullstack">
-                        <h1>Projet Fullstack</h1>
-                    </div>
+</head>
 
-                    <div class="col-lg-5 connexion" align="center">
-                        <div class="col-xs-12" align="center">
-                            
-                                <h1 id="modif2">Vous connectez</h1>
-                                    <form action="" method="POST"> 
-                                        <p><label><h3>Adresse Mail</h3></label>
-                                        <input type="text" name="NDC_2" class="text"/></p>
-                                        <p><label><h3>Mot De Passe</h3></label>
-                                        <input type="password" name="MDP_2" class="text"/></p>
-                                        <p> </p>
-                                        <input type="submit" name="lien1" value="connexion" class="button"/>
-                                    </form>
+<body>
+    <div class="container" align="center">
+        <div class="row">
 
-            <?php
-                if(empty($_POST['NDC_2']) && empty($_POST['MDP_2'])){
-                
+            <div class="col-lg-12 fullstack">
+                <h1>Projet Fullstack</h1>
+            </div>
 
-                    }else{
-                    
-                    $user = new user();
-                    $user->usersConnexion($_POST['NDC_2'] , $_POST['MDP_2']);
-                    $isconnectUS = $user->Compar($_POST['NDC_2'],$_POST['MDP_2']);
-                    if($isconnectUS){
+            <div class="col-lg-5 connexion" align="center">
+                <div class="col-xs-12" align="center">
 
-                    echo"<p><h3>vous etes connectez.</h3></p>"; 
+                    <h1 id="modif2">Vous connectez</h1>
+                    <form action="" method="POST">
+                        <p><label>
+                                <h3>Adresse Mail</h3>
+                            </label>
+                            <input type="text" name="NDC_2" class="text" /></p>
+                        <p><label>
+                                <h3>Mot De Passe</h3>
+                            </label>
+                            <input type="password" name="MDP_2" class="text" /></p>
+                        <p> </p>
+                        <input type="submit" name="lien1" value="connexion" class="button" />
+                    </form>
 
-                    $_SESSION['Connect']=true;
+                    <?php
+                    if (!empty($_POST['NDC_2']) && !empty($_POST['MDP_2'])) {
 
-                    ?><p><input type="button" name="lien1" value="redirection" onclick="self.location.href=''" style="background-color:#3cb371" style="color:white; font-weight:bold"onclick class="bouton"></p><?php
-                             
-                    }else{ 
-                        
-                        echo"<p><h3>Identifiants ou mot de passe incorrects, veuillez reessayer.</h3></p>";
-                    }
-                }
-            ?> 
-                        
-                        </div>
-                    </div>
+                        $user = new user($Bdd);
+                        $user->usersConnexion($_POST['NDC_2'], $_POST['MDP_2']);
+                        $isconnectUS = $user->Compar2($_POST['NDC_2'], $_POST['MDP_2']);
+                        if ($isconnectUS) {
 
-                    <div class="col-lg-2">
-                    </div>
+                            echo "<p><h3>vous etes connectez.</h3></p>";
 
-                    <div class="col-lg-5 inscription" align="center">
-                        <div class="col-xs-12" align="center">
-                            
-                                <h1 id="modif1">Creer votre compte</h1> 
-                                    <form action="index.php" method="POST"> 
-                                        <p><label><h3>Pseudo</h3></label>
-                                        <input type="text" name="pseudo_1" class="text"/></p>
-                                        <p><label><h3>Adresse Mail</h3></label>
-                                        <input type="text" name="mail_1" class="text"/></p>
-                                        <p><label><h3>Mot De Passe</h3></label>
-                                        <input type="password" name="MDP_1" class="text"/></p>
-                                        <p> </p>
-                                        <input type="submit" name="valider1" value="s'inscrire" class="button"/>
-                                    </form>
+                            $_SESSION['Connect'] = true;
 
-            <?php
-                if(empty($_POST['pseudo_1']) && empty($_POST['mail_1']) && empty($_POST['MDP_1'])){
-      
-                }else{ 
-                    
-                    $user = new user();
-                    $user->UsersNew($_POST['pseudo_1'] , $_POST['mail_1'] , $_POST['MDP_1']);
-                    echo"<p><h3>nouvelle utilisateur creer veuillez renter a nouveau vos identifiants.</h3></p>";
-                }
-            ?>
-                            
-                        </div>
-                    </div>
+                    ?><p><input type="button" name="lien1" value="redirection" onclick="self.location.href=''" style="background-color:#3cb371" style="color:white; font-weight:bold" onclick class="bouton"></p><?php
 
-                    <div class="col-lg-12 presentation">
-                        salut ca va
-                    </div>
+                                                                                                                                                                                                                } else {
+
+                                                                                                                                                                                                                    echo "<p><h3>Identifiants ou mot de passe incorrects, veuillez reessayer.</h3></p>";
+                                                                                                                                                                                                                }
+                                                                                                                                                                                                            }
+                                                                                                                                                                                                                    ?>
 
                 </div>
             </div>
-        </body>
+
+            <div class="col-lg-2">
+            </div>
+
+            <div class="col-lg-5 inscription" align="center">
+                <div class="col-xs-12" align="center">
+
+                    <h1 id="modif1">Creer votre compte</h1>
+                    <form action="" method="POST">
+                        <p><label>
+                                <h3>Pseudo</h3>
+                            </label>
+                            <input type="text" name="pseudo_1" class="text" /></p>
+                        <p><label>
+                                <h3>Adresse Mail</h3>
+                            </label>
+                            <input type="text" name="mail_1" class="text" /></p>
+                        <p><label>
+                                <h3>Mot De Passe</h3>
+                            </label>
+                            <input type="password" name="MDP_1" class="text" /></p>
+                        <p> </p>
+                        <input type="submit" name="valider1" value="s'inscrire" class="button" />
+                    </form>
+
+                    <?php
+                    if (empty($_POST['pseudo_1']) && empty($_POST['mail_1']) && empty($_POST['MDP_1'])) {
+                    } else {
+
+                        $user = new user($Bdd);
+                        $user->UsersNew($_POST['pseudo_1'], $_POST['mail_1'], $_POST['MDP_1']);
+                        echo "<p><h3>nouvelle utilisateur creer veuillez renter a nouveau vos identifiants.</h3></p>";
+                    }
+                    ?>
+
+                </div>
+            </div>
+
+            <div class="col-lg-12 presentation">
+                salut ca va
+            </div>
+
+        </div>
+    </div>
+</body>
+
 </html>
