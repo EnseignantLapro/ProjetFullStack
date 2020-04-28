9986f99d25a6b90deb72bd68ea385e6931f050e3
clermont80 <clermont.mailpro@gmail.com>
2020-04-28 14:16:28 +0200

    correctino de fautes d'inatention, cette version fonctionne sans probleme avec ou sans le formulaire IHM.php

--- API/API.php
+++ API/API.php
@@ -21,7 +21,7 @@ if(isset($_POST['combat'])&&isset($_POST['idagresseur'])&&isset($_POST['idvictim
         try
         {
             $bdd = new PDO('mysql:host=localhost; dbname=pfullstack; charset=utf8','root','');
-            echo “reussi”;
+            echo "reussi";
         }
         catch(Exception $e)
         {
@@ -32,23 +32,23 @@ if(isset($_POST['combat'])&&isset($_POST['idagresseur'])&&isset($_POST['idvictim
         {
             $idagresseur = $_POST['idagresseur'];
             $idvictime = $_POST['idvictime'];
-            $dataagresseur = $Bdd->query('SELECT id_entite FROM entite where id_entite="'.$idagresseur.'"'); //on verifie si les deux entités demandées existent en base
+            $dataagresseur = $bdd->query('SELECT id_entite FROM entite where id_entite="'.$idagresseur.'"'); //on verifie si les deux entités demandées existent en base
             $tabagresseur = $dataagresseur->fetch();
             $idagresseur = $tabagresseur['id_entite'];
 
-            $datavictime = $Bdd->query('SELECT id_entite FROM entite where id_entite="'.$idvictime.'"');
+            $datavictime = $bdd->query('SELECT id_entite FROM entite where id_entite="'.$idvictime.'"');
             $tabvictime = $datavictime->fetch();
             $idvictime = $tabvictime['id_entite'];
 
-            $agresseur = new Entite($idagresseur,$Bdd);
-            $victime = new Entite($idvictime,$Bdd);
+            $agresseur = new Entite($idagresseur,$bdd);
+            $victime = new Entite($idvictime,$bdd);
 
-            $agresseur->Attaquer($idagresseur,$idvictime,$Bdd);
+            Attaquer($idagresseur,$idvictime,$bdd);
 
 
             $viepersoattaquant = $agresseur->getPdv(); //on récupère la vie de l'entité attaquant et de l'entité attaquée, ce sont les informations à retourner
             $viepersovictime = $victime->getPdv();
-            $life = array('vie' => $vieperso1, 'vie2' => $vieperso2);
+            $life = array('vie' => $viepersoattaquant, 'vie2' => $viepersovictime);
 
             echo json_encode($life);
             //$jsonretour = "{_Vie:vieperso1,_Vie:vieperso2}";
