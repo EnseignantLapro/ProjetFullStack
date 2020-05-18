b43361b83fe6983e22d6676555ac8d129bb820e0
Flaquet <eflaquet@la-providence.net>
2020-05-18 14:27:13 +0200

    oui

--- API/API_Afficvie.php
+++ API/API_Afficvie.php
@@ -5,7 +5,7 @@
       $ida = 1;
       $idv = 2 ;
 
-      try{
+      try{ //connection a la basede donnée
 
             $Bdd = new PDO('mysql:host=' . $adresse . '; dbname=' . $nomBase . '; charset=utf8', '' . $userBase . '', '' . $mdpBase . '');
             
@@ -15,15 +15,15 @@
       }
 
 
-      $attaque = new Entite($ida, $Bdd);
+      $attaque = new Entite($ida, $Bdd); //on envoie id de l'attaquant dans la class etitée pour etre traiter avec la connection a la bdd
       $victime = new Entite($idv, $Bdd);
 
-      $vieattaque = $attaque->getPdv();
+      $vieattaque = $attaque->getPdv(); // on recupere la vien de l'attaquant grace a la class entitée 
       $vievictime = $victime->getPdv();
 
-      $vie = array("La vie de l'attaquant " => $vieattaque, "Vie de la victime " =>$vievictime);
+      $vie = array("La vie de l'attaquant " => $vieattaque, "Vie de la victime " =>$vievictime); //on insere la vie de l'attaquant est de la victime dans un tableau
 
       
-      echo json_encode($vie);
+      echo json_encode($vie); // on encode le tableau en json 
 
 ?>
